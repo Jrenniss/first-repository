2f7c3916cfb2a111fafaf8e70d7aade3c33e4b1c
JENNIFER RYAN - STUDENT <G00362599@gmit.ie>
2018-02-27 12:46:13 +0000

    Java Lab Week 21

---
+++ Continue.java
@@ -0,0 +1,27 @@
+/**
+Name: ContinueTest
+Description: Rewrite code as seen
+Created By: Jennifer Ryan
+Created On: 27/02/2018
+*/
+
+import java.util.Scanner;
+
+public class Continue{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int number;
+		int counter = 0;
+
+		System.out.print("Enter a number from 1 to 10: ");
+		number = input.nextInt();
+
+		while(counter <= 10){
+			++counter;
+			if (number == counter){
+				continue;
+				}
+				System.out.println(counter);
+			}
+	}
+}
---
+++ ContinueTest.java
@@ -0,0 +1,19 @@
+/**
+Name: ContinueTest
+Description: Rewrite code as seen
+Created By: Jennifer Ryan
+Created On: 27/02/2018
+*/
+
+public class ContinueTest{
+	public static void main(String[] args){
+
+		for(int count = 1; count <= 10; count++){
+			if(count == 5)
+				continue;
+
+				System.out.println(count);
+				}
+				System.out.println("\nUsed to continue to skip printing 5");
+	}
+}
