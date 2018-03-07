b99b102c8ad2118e2d5cfd0d5dbba522511420c0
JENNIFER RYAN - STUDENT <G00362599@gmit.ie>
2018-03-07 13:55:03 +0000

    Lab 8 Week 22

---
+++ Power.java
@@ -0,0 +1,39 @@
+/**
+Name: Power
+Description: includes integerPower(base,exponent) that returns the value of base to the powerof the exponant
+Created By: Jennifer Ryan
+Created On: 07/03/2018
+*/
+import java.util.Scanner;
+
+	public class Power{
+		public static void main(String[] args){
+			Scanner input = new Scanner(System.in);
+				int base;
+				int exp;
+
+
+			System.out.println("Enter base <negative to quit>: ");
+				base = input.nextInt();
+
+		while(base > 0){
+			System.out.println("Enter Exponent: ");
+				exp = input.nextInt();
+
+				int ans = integerPower(base, exp);
+				System.out.println(base+" to the "+exp+" power is "+ans);
+
+				System.out.println("Enter base <negative to quit>: ");
+					base = input.nextInt();
+		}
+	}//end main
+
+	public static int integerPower(int base, int exp){
+		int product = 1;
+
+		for(int i = 1; i <= exp; i++)
+			 product *= base;
+
+			return product;
+			}
+}//end class
---
+++ Square.java
@@ -0,0 +1,30 @@
+/**
+Name: Square
+Description: includs method to display solid square of asterisks
+Created By: Jennifer Ryan
+Created On: 07/03/2018
+*/
+
+import java.util.Scanner;
+
+	public class Square{
+		public static void main(String[] args){
+
+			Scanner input = new Scanner(System.in);
+
+			System.out.println("Enter Square Size: ");
+			int size = input.nextInt();
+
+			squareOfAsterisks(size);
+
+	}//end main
+
+	public static void squareOfAsterisks(int size){
+		 for(int row = 1; row <= size; row++){
+		 	for(int col = 1; col <= size; col++)
+		 		System.out.print("*");
+
+		 			System.out.println();
+				}
+		}
+}
